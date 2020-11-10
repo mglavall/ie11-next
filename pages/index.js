1c3e213a1636a3a8df60aa1726713de627a40964
Marsa <mgomezlavall@gmail.com>
2020-11-10 09:19:18 +0100

    add 2 more images

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,24 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Image
+        src="https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png"
+        alt="Picture of the author"
+        loading="lazy"
+        width={500}
+        height={500}
+      ></Image>
+      <Image
+        src="https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png"
+        alt="Picture of the author"
+        loading="eager"
+        width={500}
+        height={500}
+      ></Image>
       <Image
         src="https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png"
         alt="Picture of the author"
